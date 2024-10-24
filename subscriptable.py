a1fcb844b1c75ec402fca6cfde53e9bf369a6569
Eric <ericv8v9s@outlook.com>
2024-10-23 21:07:01 -0400

    added subscriptable map and zip

---
+++ subscriptable.py
@@ -0,0 +1,56 @@
+import builtins
+
+
+builtins_map = map
+builtins_zip = zip
+
+
+def _fill_items(items, iterable, stop):
+	if stop is None or stop >= len(items):
+		for _, val in builtins_zip(range(stop - len(items) + 1), iterable):
+			items.append(val)
+
+
+class Subscriptable:
+	def __init__(self, source):
+		self._source = source
+		self._items = []
+
+	def __getitem__(self, index):
+		if isinstance(index, int):
+			_fill_items(self._items, self._source, index)
+		elif isinstance(index, slice):
+			_fill_items(self._items, self._source, index.stop)
+		else:
+			raise TypeError
+		return self._items[index]
+
+	def __len__(self):
+		_fill_items(self._items, self._source, None)
+		return len(self._items)
+
+	def __getattr__(self, name):
+		return self._source.__getattribute__(name)
+
+
+class SubscriptableMap(Subscriptable):
+	@staticmethod
+	def patch():
+		builtins.map = SubscriptableMap
+
+	def __init__(self, function, iterable, *iterables):
+		super().__init__(builtins_map(function, iterable, *iterables))
+
+
+class SubscriptableZip(Subscriptable):
+	@staticmethod
+	def patch():
+		builtins.zip = SubscriptableZip
+
+	def __init__(self, *iterables, strict=False):
+		super().__init__(builtins_zip(*iterables, strict=strict))
+
+
+def patch_all():
+	builtins.map = SubscriptableMap
+	builtins.zip = SubscriptableZip
